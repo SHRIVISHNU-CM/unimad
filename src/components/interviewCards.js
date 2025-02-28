c411b8bb74e6f37c2bac48d686a2e754038ca59f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-03-01 00:36:04 +0530

    card

--- src/components/interviewCards.js
+++ src/components/interviewCards.js
@@ -1,16 +1,23 @@
 "use client";
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoIosRefresh } from "react-icons/io";
 
 
 function InterviewCards({ title, company }) {
+     const [isMounted, setIsMounted] = useState(false);
+    
+        useEffect(() => {
+            setIsMounted(true);
+        }, []);
+    
+        if (!isMounted) return null;
     return (
         <>
             <div className="bg-[#FFFFFF] rounded-[4px] w-[356px] h-[73px] py-[10px] pr-[24px] pl-[16px] flex justify-between items-center shadow-lg">
                 <div className=''>
                     <h3 className="text-[14px] font-[600]">{title}</h3>
-                    <p className="text-gray-500 text-[14px] font-[400]">{company}</p>
+                    <p className="text-[#808080] text-[14px] font-[400]">{company}</p>
                 </div>
 
                 <button className='flex items-center gap-[4px] bg-[#346DE0] text-white text-[12px] w-[76px] h-[24px] rounded-[4px] py-[4px] px-[8px]'>
