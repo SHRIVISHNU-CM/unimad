808124eda4d8402f820359ac9db0008056ec2011
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-03-02 22:49:39 +0530

    image

--- src/components/inputField.js
+++ src/components/inputField.js
@@ -1,19 +1,28 @@
 "use client";
 
 import React from "react";
-import { IoSend } from "react-icons/io5";
-import { CgProfile } from "react-icons/cg";
+import Image from "next/image";
 
 function InputField() {
   return (
     <div className=" fixed bottom-3">
       <div className="w-[1130px] h-[52px] px-4 py-2 bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-[42px] flex justify-around items-center">
-        <CgProfile className="text-[24px] text-[#346DE0]"/>
+        <Image
+          src="/images/Group.png"
+          width={40}
+          height={40}
+          alt="Group"
+          className=" text-[#346DE0]" />
         <input
           type="text"
           className="w-full border-none  outline-none"
         />
-        <IoSend className="text-[24px] text-[#346DE0]"/>
+        <Image
+          src="/images/send.png"
+          width={24}
+          height={24}
+          alt="Send"
+          className="text-[#346DE0]" />
       </div>
 
     </div>
