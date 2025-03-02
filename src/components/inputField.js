bf09aa90de76a5b7f5726df7905a314b87741af0
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2025-03-02 14:54:13 +0530

    fixed

--- src/components/inputField.js
+++ src/components/inputField.js
@@ -6,7 +6,7 @@ import { CgProfile } from "react-icons/cg";
 
 function InputField() {
   return (
-    <div className=" absolute bottom-3">
+    <div className=" fixed bottom-3">
       <div className="w-[1130px] h-[52px] px-4 py-2 bg-white border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-[42px] flex justify-around items-center">
         <CgProfile className="text-[24px] text-[#346DE0]"/>
         <input
